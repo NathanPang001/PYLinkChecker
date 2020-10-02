3ae6d637103935629346782b58d6c66451483388
Mamadou Diallo <mamadou.diallo@hotmail.com>
2020-10-02 11:38:55 -0400

    Update PYLinkChecker.py
    Adding colors to his project

--- PYLinkChecker.py
+++ PYLinkChecker.py
@@ -9,11 +9,12 @@ import urllib3
 #import requests
 
 http = urllib3.PoolManager()
-
+from colorama import init, Fore
+init(convert=True)
 #Checking for number of arguments with if/elif
 if len(sys.argv) == 1:
-    print("No arguments, please enter either -file (filename), -url (link), or -v")
-    print("Windows and Unix style commands also work, --v, /v, etc")
+    print(Fore.RED + "No arguments, please enter either -file (filename), -url (link), or -v"+ Fore.RESET)
+    print(Fore.RED + "Windows and Unix style commands also work, --v, /v, etc"+ Fore.RESET)
 
 elif len(sys.argv) == 3:
 
@@ -28,11 +29,11 @@ elif len(sys.argv) == 3:
 
                 for line in lines:
                     if ('http://' in line or 'https://' in line):
-                        print (line , "is a valid link")
+                        print (line , Fore.GREEN +  "is a valid link" + Fore.RESET)
                     else:
-                        print (line, "is not a valid link")
+                        print (line, Fore.RED + "is not a valid link" + Fore.RESET)
         except:
-            print("Sorry, file not found")
+            print(Fore.RED + "Sorry, file not found" + Fore.RESET)
         
      #URL Checker   
     elif(sys.argv[1] == "-url" or sys.argv[1] == "--url" or sys.argv[1] == "/url"):
@@ -50,13 +51,13 @@ elif len(sys.argv) == 3:
             else:
                 print(sys.argv[2], ": unknown")
         except:
-             print("Sorry, the link is broken, please fix it and try again!")
+             print(Fore.RED + "Sorry, the link is broken, please fix it and try again!" + Fore.RESET)
 
 #Version Option                  
 elif(sys.argv[1] == "-v" or sys.argv[1] == "--v" or sys.argv[1] == "/v"):
     print("This is version 0.1 of the PYLinkChecker")
     
 elif len(sys.argv) == 2:
-    print("Not enough entered, please enter the name of a file or a url")
+    print(Fore.RED + "Not enough entered, please enter the name of a file or a url" + Fore.RESET)
 
 print("END")  
