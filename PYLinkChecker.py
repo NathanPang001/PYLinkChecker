20a83ab6daf7ea1b1f21e903225702e68a8a63df
Nathan Pang <npang@myseneca.ca>
2020-10-24 22:44:48 -0400

    Editing PYLinkChecker.py for better readability and functionality:
    *Changing 'g' variable to goodCheck for better readability
    *Changing 'x' variable to errorCode for better use in the future
    *Removing 'regex' variable and adding it to the re.findall because its not used again in the code
    *Changing version from 0.3 to 0.5
    *Changing date to most recent date
    *Removing optional requirements
    *Changing version from 0.3 to 0.5
    *Added function 'FileNotFound' to reduce code duplication

--- PYLinkChecker.py
+++ PYLinkChecker.py
@@ -1,7 +1,6 @@
 #Nathan Pang
 #108660184
-#25/09/2020
-#optional requirements: version and supporting both Windows and Unix Style command line args
+#24/10/2020
 
 import sys
 import urllib3
@@ -10,21 +9,22 @@ http = urllib3.PoolManager()
 from colorama import init, Fore
 init(convert=True)
 
-regex = 'https?:\/\/[=a-zA-Z0-9\_\/\?\&\.\-]+'
+def FileNotFound():
+    return print(Fore.RED + "Sorry, file not found" + Fore.RESET)
 
-x = 0
+errorCode = 0
 if len(sys.argv) == 1:
     print(Fore.RED + "No arguments, please enter either -file (filename), -url (link), or -v"+ Fore.RESET)
     print(Fore.RED + "Windows and Unix style commands also work, --v, /v, etc"+ Fore.RESET)
 
 elif len(sys.argv) == 3 or len(sys.argv) == 4:
 
-    g = 0
+    goodCheck = 0
     if (len(sys.argv) == 4):
         if (sys.argv[3]) == "--good":
-            g = 1
+            goodCheck = 1
         elif (sys.argv[3]) == "--bad":
-            g = 2
+            goodCheck = 2
 
     if(sys.argv[1] == "-file" or sys.argv[1] == "--file" or sys.argv[1] == "/file"):
         print("File Checker")
@@ -36,25 +36,25 @@ elif len(sys.argv) == 3 or len(sys.argv) == 4:
 
                 #Using regex to find urls then slicing off the first and last 2 characters to only display URL
                 if ('http://' in line or 'https://' in line):
-                    if(g == 0 or g == 1):
+                    if(goodCheck == 0 or goodCheck == 1):
                         print(Fore.GREEN + str(re.findall(r'https?://[^\s<>"].[^\s<>"]+', line))[2:-2] + " is a valid link" + Fore.RESET)
                 elif ('www.' in line and '://' not in line):
                   
-                    if(g == 0 or g == 2):
+                    if(goodCheck == 0 or goodCheck == 2):
                         print(Fore.RED + str(re.findall(r'www.[^\s<>"].[^\s<>"]+', line))[2:-2] + " is not a valid link" + Fore.RESET)
-                        x = 1
+                        errorCode = 1
                 else:
                     if ("<" and ">" in line): #covers situation involving urls within html tags with misspelled http/https
-                        if(g == 0 or g == 2):
+                        if(goodCheck == 0 or goodCheck == 2):
                             print(Fore.RED + str(re.findall(r'>(.*?)<', line))[2:-2] + " is not a valid link" + Fore.RESET)
-                            x = 1
+                            errorCode = 1
                     else:
-                        if(g == 0 or g == 2):
+                        if(goodCheck == 0 or goodCheck == 2):
                             print(Fore.RED + line + " is not a valid link" + Fore.RESET)
-                            x = 1
+                            errorCode = 1
         except:
-            print(Fore.RED + "Sorry, file not found" + Fore.RESET)
-            x = 1
+            FileNotFound()
+            errorCode = 1
           
     elif(sys.argv[1] == "-url" or sys.argv[1] == "--url" or sys.argv[1] == "/url"):
         print("URL Checker")
@@ -64,13 +64,13 @@ elif len(sys.argv) == 3 or len(sys.argv) == 4:
                 print(sys.argv[2], ": good")
             elif response.status == 400 or response == 404:
                 print(sys.argv[2], ": bad")
-                x = 1
+                errorCode = 1
             else:
                 print(sys.argv[2], ": unknown")
-                x = 1
+                errorCode = 1
         except:
              print(Fore.RED + "Sorry, the link is broken, please fix it and try again!" + Fore.RESET)
-             x = 1
+             errorCode = 1
 
     if(sys.argv[1] == "-ignore" or sys.argv[1] == "--ignore" or sys.argv[1] == "/ignore"):
         print("File Ignore")
@@ -83,16 +83,16 @@ elif len(sys.argv) == 3 or len(sys.argv) == 4:
                     pass
                 #Using regex to find urls then slicing off the first and last 2 characters to only display URL
                 elif ('http://' in line or 'https://' in line):
-                    if(g == 0 or g == 1):
+                    if(goodCheck == 0 or goodCheck == 1):
                         str = re.findall('https?://[^\s<>"].[^\s<>"]+', line)
                         ignoreURLs += str       
         except:
-            print(Fore.RED + "Sorry, file not found" + Fore.RESET)
-            x = 1
+            FileNotFound()
+            errorCode = 1
         try:
             with open(sys.argv[3], 'r') as f:
                 #find urls in test.txt
-                lines = re.findall(regex, f.read())
+                lines = re.findall('https?:\/\/[=a-zA-Z0-9\_\/\?\&\.\-]+', f.read())
             #find all to be ignored urls
             tobeIgnored = [j for i in ignoreURLs for j in lines if i in j or j in i]
             
@@ -101,18 +101,18 @@ elif len(sys.argv) == 3 or len(sys.argv) == 4:
             for ni in notIgnored:
                 print(Fore.GREEN + ni + " is a valid link" + Fore.RESET)
         except:
-            print(Fore.RED + "Sorry, file not found" + Fore.RESET)
-            x = 1
+            FileNotFound()
+            errorCode = 1
 
 #Version Option                  
 elif(sys.argv[1] == "-v" or sys.argv[1] == "--v" or sys.argv[1] == "/v"):
-    print("This is version 0.3 of the PYLinkChecker")
+    print("This is version 0.5 of the PYLinkChecker")
     
 elif len(sys.argv) == 2:
     print(Fore.RED + "Not enough entered, please enter the name of a file or a url" + Fore.RESET)
-    x = 1
+    errorCode = 1
 
-if(x == 1):
+if(errorCode == 1):
     sys.exit(1)
 else:
     sys.exit(0)
